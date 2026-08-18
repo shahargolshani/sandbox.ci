9204952800b35e4f63a6a653598a5caffefc5991
Shahar Golshani <sgolshan@redhat.com>
2026-08-18 15:56:02 +0300

    add dummy test comment

--- plugins/modules/dummy_alert.ps1
+++ plugins/modules/dummy_alert.ps1
@@ -1,7 +1,7 @@
 #!powershell
 # Copyright: (c) 2026, Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-
+# Test integration target detection.
 #AnsibleRequires -CSharpUtil Ansible.Basic
 
 $spec = @{
